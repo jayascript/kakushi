ec66ca362d9cdb87a0d4e87fda5651faeaca04a4
Jayascript <39142547+jayascript@users.noreply.github.com>
2020-08-20 22:04:05 -0400

    Add second Screen() for monitor 2

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -148,6 +148,28 @@ screens = [
             24,
         ),
     ),
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %H:%M:%S'),
+                widget.QuickExit(),
+            ],
+            24,
+        ),
+    ),
 ]
 
 # Drag floating layouts.
