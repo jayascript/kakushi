4c710915a91b0dc5507fe0c52c313eda4b7ddc2f
Jayascript <39142547+jayascript@users.noreply.github.com>
2020-08-21 23:36:33 -0400

    Update Rofi to match fairyfloss theme

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# purple #800080
-# blue-grey #2f343f
-bg_color=#2f343f
-text_color=#e5e5e5
-htext_color=#ff69b4
+bg_color=#716799
+text_color=#C2FFDF
+htext_color=#F1568E
 bg_transparent=#95800080
-bg_no_color=#00800080
+bg_no_color=#00716799
 
-rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -monitor "eDP-1" -bw 0 -color-window "$bg_transparent, $bg_transparent, $bg_transparent" -color-normal "$bg_no_color, $text_color, $bg_no_color, $bg_no_color, $htext_color" -font "Raleway 18"
+rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -monitor "eDP-1" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_no_color, $text_color, $bg_no_color, $bg_no_color, $htext_color" -font "Raleway 18"
