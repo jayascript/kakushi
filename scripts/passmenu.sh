7d45eb85c68b9cbb0cb651a81e065348f908f7da
Jayascript <6750308-jayascript@users.noreply.gitlab.com>
2021-03-12 16:53:21 -0500

    [passmenu.sh] theme passmenu

--- scripts/passmenu.sh
+++ scripts/passmenu.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+bg_color=#ffffff
+text_color=#F1568E
+
+first_alternating_row_color=#f8f8f2
+second_alternating_row_color=#ffffff
+
+active_text_color=#42395d
+active_bg_color=#ffb8d1
+
 shopt -s nullglob globstar
 
 typeit=0
@@ -13,7 +22,7 @@ password_files=( "$prefix"/**/*.gpg )
 password_files=( "${password_files[@]#"$prefix"/}" )
 password_files=( "${password_files[@]%.gpg}" )
 
-password=$(printf '%s\n' "${password_files[@]}" | rofi -dmenu -p "Password")
+password=$(printf '%s\n' "${password_files[@]}" | rofi -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$first_alternating_row_color, $text_color, $second_alternating_row_color, $active_bg_color, $active_text_color" -font "Monofur 18" -dmenu -p "Password")
 
 [[ -n $password ]] || exit
 
