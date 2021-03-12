f4db7d0bdf0f400df3a1fe91be5753ec5feb3136
Jayascript <6750308-jayascript@users.noreply.gitlab.com>
2021-03-12 16:13:09 -0500

    [rofi.sh] update to use monofur font

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -6,4 +6,4 @@ htext_color=#F1568E
 bg_transparent=#95800080
 bg_no_color=#00716799
 
-rofi -show run -lines 3 -eh 2 -width 100 -padding 500 -monitor "eDP-1" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_no_color, $text_color, $bg_no_color, $bg_no_color, $htext_color" -font "Raleway 18"
+rofi -show run -lines 3 -eh 2 -width 100 -padding 500 -monitor "eDP-1" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_no_color, $text_color, $bg_no_color, $bg_no_color, $htext_color" -font "Monofur 18"
